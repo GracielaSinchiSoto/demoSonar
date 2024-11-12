41207c10adb16436c7e58812f3d277142829bfe8
Graciela Sinchi <103075285+GracielaSinchiSoto@users.noreply.github.com>
2024-11-12 15:43:48 -0500

    bat updated

--- DemoSonar/run_sonnar.bat
+++ DemoSonar/run_sonnar.bat
@@ -1,3 +1,3 @@
-dotnet sonarscanner begin /o:"usuariotmp001" /k:"usuariotmp001_demoSonar" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716" /d:sonar.scanner.scanAll=false
+dotnet sonarscanner begin /o:"gracielasinchisoto" /k:"GracielaSinchiSoto_demoSonar" /d:sonar.host.url="https://sonarcloud.io"   /d:sonar.token="a40601136522f43274c6a81e78483470c41cc748"
 dotnet build
-dotnet sonarscanner end /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716"
+dotnet sonarscanner end /d:sonar.token="a40601136522f43274c6a81e78483470c41cc748"
